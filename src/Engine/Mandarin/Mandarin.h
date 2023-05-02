9db82ccdf986c91e1df616460b405156fa5cdabc
Jim Chen <10269287+jimchen5209@users.noreply.github.com>
2023-05-02 22:00:47 +0800

    fix: add missing cstdint import

--- src/Engine/Mandarin/Mandarin.h
+++ src/Engine/Mandarin/Mandarin.h
@@ -28,6 +28,7 @@
 #include <map>
 #include <string>
 #include <vector>
+#include <cstdint>
 
 namespace Formosa {
 namespace Mandarin {
--- src/Engine/ParselessPhraseDB.cpp
+++ src/Engine/ParselessPhraseDB.cpp
@@ -25,6 +25,7 @@
 
 #include <cassert>
 #include <cstring>
+#include <cstdint>
 
 namespace McBopomofo {
 
